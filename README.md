549f486d33d3d1a963f909dfc5e2783d8ccf82f4
chrissyalbert <chrissy_albert@yahoo.com>
2020-08-11 15:47:58 -0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Chrissy Albert Porfolio Website
-This website displays the projects that I have been developing. 
---
+++ precache-manifest.e1de8e36c05298d899951c18664e05ec.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aac810d8e1c4880a3ea2e5a14321f63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4845e9e901bc8606a6ba",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "0a89cb8ccaf4c29ca29e",
+    "url": "/static/css/main.6905ff36.chunk.css"
+  },
+  {
+    "revision": "4845e9e901bc8606a6ba",
+    "url": "/static/js/2.218c7605.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.218c7605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a89cb8ccaf4c29ca29e",
+    "url": "/static/js/main.9b932a49.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "17d8d607b7d41a3305218ea014424e69",
+    "url": "/static/media/Albert-Chrissy-7089-square.17d8d607.jpg"
+  },
+  {
+    "revision": "a1dd18de6c51cefa640c4b22eef0ca78",
+    "url": "/static/media/CSS_documentation.a1dd18de.png"
+  },
+  {
+    "revision": "8d2daafd3fe4b8a666b39aa03e5f2c96",
+    "url": "/static/media/black triangles.8d2daafd.png"
+  },
+  {
+    "revision": "60dbd9de8bd5020750302cccc4528fba",
+    "url": "/static/media/calculator.60dbd9de.png"
+  },
+  {
+    "revision": "7c0856d2b0cd911d13d9cdb7c7dfec0c",
+    "url": "/static/media/college.7c0856d2.png"
+  },
+  {
+    "revision": "ebe238e764e0a2e9d0179d5891bf01ee",
+    "url": "/static/media/covid.ebe238e7.png"
+  },
+  {
+    "revision": "06980d918b3bd7a2746be019b2cafd0d",
+    "url": "/static/media/drumMachine.06980d91.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "1cfd480b6c60a0c105a83477667cda1d",
+    "url": "/static/media/markdown.1cfd480b.png"
+  },
+  {
+    "revision": "a01b64c3646fc5290aec6a5c0127f361",
+    "url": "/static/media/minecraftSurvey.a01b64c3.png"
+  },
+  {
+    "revision": "465f29440245b8688a7def67effe5166",
+    "url": "/static/media/newEarth.465f2944.png"
+  },
+  {
+    "revision": "8818913edc569d9a6a4973d0a8236018",
+    "url": "/static/media/pomodoro.8818913e.png"
+  }
+]);
